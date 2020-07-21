93ff105b02669c58807a87488b6d62d59e1980ab
Its_praveen_jd <48998620+praveenjangid2000@users.noreply.github.com>
2020-07-22 01:24:27 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { AppRegistry, View } from 'react-native';
+import Routes from './screen/Routes.js'
+
+class App extends Component {
+   render() {
+      return (
+         <Routes />
+      )
+   }
+}
+export default App
